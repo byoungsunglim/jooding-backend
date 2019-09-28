9613dfc76a330a20b24c3fef5558fc4739a815b3
ryanlim95 <ryanlim1995@gmail.com>
2019-09-27 19:52:51 -0700

    questionnaires debugging

--- apis/questionnaires.py
+++ apis/questionnaires.py
@@ -4,25 +4,23 @@ import json
 import firebase_admin
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from firebase_admin import credentials, firestore
+from firebase_admin import firestore
 from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
-
-# Use a service account
-cred = credentials.Certificate('firebase-service-key.json')
-firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 @api_view(["GET"])
 def questionnaires(request):
+    print("#")
     docs = db.collection(u'questionnaires').stream()
 
     results = []
 
+    print(docs)
     for doc in docs:
-        results.append(doc.to_dict())
+        print(u'{} => {}'.format(doc.id, doc.to_dict()))
 
     response = {
         "status_code": 200,
--- apis/users.py
+++ apis/users.py
@@ -4,14 +4,10 @@ import json
 import firebase_admin
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from firebase_admin import credentials, firestore
+from firebase_admin import firestore
 from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
-
-# Use a service account
-cred = credentials.Certificate('firebase-service-key.json')
-firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
